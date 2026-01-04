6c2006b943943124c432969d075bb5e5f6013007
Otman Ouali <otto.ouali@gmail.com>
2026-01-04 20:38:21 +1000

    Removing react import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   site: 'https://ottowoolf.github.io',
   base: '/OPortfolio', // This adds the prefix to all generated links
 });
