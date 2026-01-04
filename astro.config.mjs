3d9a252049dbf9b4ac25a9a4a9567f94336f7e89
Otman Ouali <otto.ouali@gmail.com>
2026-01-04 21:27:48 +1000

    Reverting url name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://oportfolio.github.io'
+  site: 'https://ottowoolf.github.io'
 });
