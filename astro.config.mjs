5decfa006f3497426a6bdcc7f14be4b96e1a5649
Otman Ouali <otto.ouali@gmail.com>
2026-02-11 15:01:53 +1000

    Adding Domain Name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ottowoolf.github.io'
+  site: 'https://otmandesigns.com'
 });
